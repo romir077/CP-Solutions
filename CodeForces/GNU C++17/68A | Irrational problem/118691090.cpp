b07d66921de345c78388c1a3616e4f5cf6fa977a
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:46:52 +0530

    Committing files

---
+++ CodeForces/GNU C++17/68A | Irrational problem/118691090.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    dia dib dic dix diy diz
+    int p=min(min(a,b),min(c,x));
+    if(p-y>=0 && z>=p) cout<<p-y<<"\n";
+    else if(z<p) cout<<z-y+1<<"\n";
+    else cout<<0<<"\n";
+    return 0;
+}
