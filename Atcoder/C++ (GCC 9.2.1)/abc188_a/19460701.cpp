2713f3d25c48546b5daaac8903c98a1134415386
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 13:35:28 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc188_a/19460701.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if((abs(x-y))<=2){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
