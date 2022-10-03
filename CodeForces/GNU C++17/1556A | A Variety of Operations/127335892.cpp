15024096f23d9ed32d6aed1b54b7a8393ff6710f
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:42:42 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1556A | A Variety of Operations/127335892.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;
+}
+ 
+void solve(){
+    dic di(d)
+    if((c+d)%2==1) {cout<<"-1\n";return;}
+    else{
+        if(c==d) {
+            if(c!=0)cout<<"1\n";
+            else cout<<"0\n";
+        }
+        else cout<<"2\n";
+    }
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
