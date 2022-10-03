417c00a3c5c5a4a740231db3f7e71774d3873003
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:43:22 +0530

    Committing files

---
+++ CodeForces/GNU C++17/456A | Laptops/126385361.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    din
+    vpii a(n);
+    repin cin>>a[i].first>>a[i].second;
+    sort(all(a));
+    rep(i,0,n-1){
+        if(a[i].second>a[i+1].second){cout<<"Happy Alex\n";return 0;}
+    }
+    cout<<"Poor Alex\n";
+    return 0;
+}
