f5cf7b515a8ee1282fe2218591afc5ff8bb2db50
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:46:39 +0530

    Committing files

---
+++ CodeForces/GNU C++17/579A | Raising Bacteria/118794643.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    din
+    int sum=0;
+    while(n>0){
+        if(n%2==1) {sum++;n--;}
+        else n/=2;
+    }
+    cout<<sum;
+    return 0;
+}
