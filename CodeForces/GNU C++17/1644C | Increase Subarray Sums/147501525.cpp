b453b9e3898e34599a721d351839320133d02158
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:32:32 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1644C | Increase Subarray Sums/147501525.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+#define MOD 1000000007
+#define el cout<<endl;
+using namespace std;
+ 
+ 
+int power( int N, int m){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(m > 0){if((m & 1) == 1){sum *= power;sum%=MOD;}
+    power = power * power;m = m >> 1;power%=MOD;}
+    return sum%MOD;
+}
+ 
+int mod_inverse(int x){
+    int result = 1;
+    return power(x,MOD-2);
+}
+ 
+void solve(){
+    din dix
+    V a(n);
+    take(a,n);
+    V b(n+1,INT_MIN);
+    V c(n+1,0);
+    c[0]=0;
+    rep(i,1,n+1){
+        c[i]=c[i-1]+a[i-1];
+    }
+    b[0]=0;
+    rep(i,1,n+1){
+        rep(j,i,n+1){
+            b[i]=max(b[i],c[j]-c[j-i]);
+        }
+    }
+    rep(i,0,n+1){
+        int ans=INT_MIN;
+        int y=i*x;
+        rep(j,0,n+1){
+            ans=max(ans,b[j]+min(j*x,y));
+        }
+        cout<<ans<<" ";
+    }
+    cout<<"\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
