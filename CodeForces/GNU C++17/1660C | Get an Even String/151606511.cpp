bb69d50b35d0827a9852fb12654a63f7b5e10ef7
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:30:48 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1660C | Get an Even String/151606511.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+#define MOD 1000000007
+#define el cout<<endl;
+using namespace std;
+ 
+ 
+int power( int N, int m){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(m > 0){if((m & 1) == 1){sum *= power;sum%=MOD;}
+    power = power * power;m = m >> 1;power%=MOD;}
+    return sum%MOD;
+}
+ 
+int mod_inverse(int x){
+    int result = 1;
+    return power(x,MOD-2);
+}
+ 
+void solve(){
+    sin
+    int ans=0;
+    int n=s.length();
+    set<char>st;
+    int a=0;
+    repin{
+        int f=0;
+        if(st.find(s[i])!=st.end()){
+            ans+=st.size()-1;
+            st.clear();
+            f=1;
+        }
+        if(f==0)st.insert(s[i]);
+    } 
+    if(st.size()){
+        ans+=st.size();
+    }
+    if(n==1) ans=1;
+    cout<<ans<<"\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
