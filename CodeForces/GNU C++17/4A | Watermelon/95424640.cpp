782b33975facec401d27ab0f2f22679810e0158b
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:55:06 +0530

    Committing files

---
+++ CodeForces/GNU C++17/4A | Watermelon/95424640.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int watermelon;
+ cin>>watermelon;
+ if(watermelon%2==1){
+  cout<<"NO\n";
+ }
+ else if(watermelon==2){
+     cout<<"NO\n";
+ }
+ else{
+  cout<<"YES\n";
+ }
+ return 0;
+}
