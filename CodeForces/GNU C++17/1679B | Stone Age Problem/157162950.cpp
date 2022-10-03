2c9a5d896f9097cb76a0bd0c7c0f49709dc70c81
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 10:42:35 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1679B | Stone Age Problem/157162950.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+#define MOD 1000000007
+#define el cout<<endl;
+using namespace std;
+ 
+ 
+int power( int N, int m){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(m > 0){if((m & 1) == 1){sum *= power;sum%=MOD;}
+    power = power * power;m = m >> 1;power%=MOD;}
+    return sum%MOD;
+}
+ 
+int mod_inverse(int x){
+    int result = 1;
+    return power(x,MOD-2);
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    din di(q)
+    V a(n);
+    take(a,n);
+    int sum=0;
+    repin sum+=a[i];
+    map<int,int>mp;
+    int p=-1;
+    rep(i,0,q){
+        dix
+        if(x==1){
+            dib dic
+            if(p==-1){
+                sum+=c-a[b-1];
+                a[b-1]=c;
+                cout<<sum<<"\n";
+            }
+            else{
+                if(mp.find(b)!=mp.end()){
+                    sum+=c-mp[b];
+                    mp[b]=c;
+                    cout<<sum<<"\n";
+                }
+                else{
+                    sum+=c-p;
+                    mp[b]=c;
+                    cout<<sum<<"\n";
+                }
+            }
+        }
+        else{
+            mp.clear();
+            dib
+            p=b;
+            sum=n*b;
+            cout<<b*n<<"\n";
+        }
+    }
+    return 0;
+}
