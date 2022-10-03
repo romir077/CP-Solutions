708fdc51be22fd0561d743171c6a1d8918f0109a
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 10:41:29 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1684B | Z mod X = C/157695376.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+#define MOD 1000000007
+#define el cout<<endl;
+using namespace std;
+ 
+ 
+int power( int N, int m){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(m > 0){if((m & 1) == 1){sum *= power;sum%=MOD;}
+    power = power * power;m = m >> 1;power%=MOD;}
+    return sum%MOD;
+}
+ 
+int mod_inverse(int x){
+    int result = 1;
+    return power(x,MOD-2);
+}
+ 
+void solve(){
+    V g(3);
+    cin>>g[0]>>g[1]>>g[2];
+    // sort(all(g),greater<int>());
+    int a=g[0],b=g[1],c=g[2];
+    int z=c;
+    int y=1;
+    while(y<=z)y*=z;
+    y+=b;
+    int x=1;
+    x=2*y;
+    x+=a;
+    // while(y%z!=0)y+=z;
+    cout<<x<<" "<<y<<" "<<z<<"\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
