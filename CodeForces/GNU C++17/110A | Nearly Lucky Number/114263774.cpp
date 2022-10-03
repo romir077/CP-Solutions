0cba50de2900eb0b72269f23dc8af9795c0f6d7a
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:49:42 +0530

    Committing files

---
+++ CodeForces/GNU C++17/110A | Nearly Lucky Number/114263774.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    din
+    int sum=0;
+    while(n>0){
+        if(n%10==4 || n%10==7) sum++;
+        n/=10;
+    }
+    if(sum==7 || sum==4) cout<<"YES\n";
+    else{cout<<"NO\n";}
+    return 0;
+}
