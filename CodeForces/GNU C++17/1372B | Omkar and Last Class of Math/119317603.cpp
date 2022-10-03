8300c6ff6e5c01848a47aa19bab8c5192312f789
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:46:04 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1372B | Omkar and Last Class of Math/119317603.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+void solve(){
+    din
+    if(n%2==0) cout<<n/2<<" "<<n/2<<"\n";
+    else{
+        int x=n;
+        for(int i=3;i*i<=n;i+=2){
+            if(n%i==0) {x=i;break;}
+        }
+        cout<<n/x<<" "<<n-n/x<<"\n";
+    }
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
