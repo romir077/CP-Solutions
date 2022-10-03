0fb2e6687676a1977bae3d6bac90ec94c5fd71ef
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:42:21 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1465B | Fair Numbers/127442195.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;
+}
+ 
+void solve(){
+    din
+    while(1){
+        int a=n;
+        while(a){
+            if(a%10!=0){if((n%(a%10))!=0){n++;break;}else {a/=10;continue;}}
+            else a/=10;
+ 
+        }
+        if(a==0){cout<<n<<"\n";break;}
+    }
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
