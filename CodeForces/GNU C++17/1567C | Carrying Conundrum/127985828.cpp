86f8f28da935ab820a4f66abcfa9bd51343671fe
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:42:04 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1567C | Carrying Conundrum/127985828.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;
+}
+ 
+void solve(){
+    din
+    
+    string s=to_string(n);
+    string r="",a="";
+    rep(i,0,s.length()){
+        if(i%2==0){
+            r+=s[i];
+        }
+        else a+=s[i];
+    }
+    int x=((r!="")?stoi(r):0),y=((a!="")?stoi(a):0);
+    cout<<(x+1)*(y+1)-2<<"\n";
+    
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
