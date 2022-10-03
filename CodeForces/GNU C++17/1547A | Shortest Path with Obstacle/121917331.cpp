d1873fcfd59d53ba1b75616793af1a4263004823
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:45:07 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1547A | Shortest Path with Obstacle/121917331.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+void solve(){
+    dia dib
+    dix diy
+    di(p) di(q)
+    if((a==x && x==p && (q-y)*(q-b)<0)||(b==y && y==q && (p-x)*(p-a)<0)){
+        cout<<abs(a-x)+abs(b-y)+2<<"\n";
+    }
+    else{
+        cout<<abs(a-x)+abs(b-y)<<"\n";
+    }
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
