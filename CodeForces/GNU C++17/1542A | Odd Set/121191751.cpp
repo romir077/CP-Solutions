da8d4d7338fc0d6f8dcf80c5ef0cf745b2c4f2b1
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:45:26 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1542A | Odd Set/121191751.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+void solve(){
+    din
+    V arr(2*n);
+    int o=0,e=0;
+    rep(i,0,2*n) {cin>>arr[i];
+        if(arr[i]%2==0) e++;
+        if(arr[i]%2==1) o++;
+    }
+    if(o==e) cout<<"Yes\n";
+    else cout<<"No\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
