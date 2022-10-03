ddbb2e9276348704213365feccdea43be7caf2dd
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:53:21 +0530

    Committing files

---
+++ CodeForces/GNU C++17/265A | Colorful Stones (Simplified Edition)/106272425.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a) for(int i=0;i<a;i++)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define din di(n)
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    sin string t;cin>>t;
+    int i=0;
+    rep(j,t.length()){
+        if(s[i]==t[j]){
+            i++;
+        }
+    }
+    cout<<i+1<<"\n";
+    return 0;
+}
