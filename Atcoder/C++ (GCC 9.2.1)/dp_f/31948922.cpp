e6f1f704aa3b5230399c7e777f33b1694c11ee56
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 13:34:24 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_f/31948922.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+#define MOD 1000000007
+#define el cout<<endl;
+using namespace std;
+
+
+int power( int N, int m){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(m > 0){if((m & 1) == 1){sum *= power;sum%=MOD;}
+    power = power * power;m = m >> 1;power%=MOD;}
+    return sum%MOD;
+}
+
+int mod_inverse(int x){
+    int result = 1;
+    return power(x,MOD-2);
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    sin string r;cin>>r;
+    int n=s.length(),m=r.length();
+    vector<V>dp(n+1,V(m+1,0));
+    rep(i,1,n+1){
+        rep(j,1,m+1){
+            if(s[i-1]==r[j-1]){
+                dp[i][j]=dp[i-1][j-1]+1;
+            }
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    string ans="";
+    int x=n,y=m;
+    while(x>0 && y>0){
+        if(s[x-1]==r[y-1]){
+            x--;y--;
+            ans+=s[x];
+        }
+        else{
+            if(dp[x][y]==dp[x-1][y]){
+                x--;
+            }
+            else y--;
+        }
+    }
+    reverse(all(ans));
+    cout<<ans<<"\n";
+
+    return 0;
+}
