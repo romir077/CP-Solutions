596cb855093a71ab1b10377e3876af9088bf828b
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 13:35:09 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc194_a/20695570.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+
+signed main()
+{
+    dia dib
+    if(b>=8){
+        if(a+b>=15) cout<<"1\n";
+        else if(a+b<15 && a+b>=10) cout<<"2\n";
+        else cout<<"3\n";
+    }
+    else if(b<8 && b>=3){
+        if(a+b>=10) cout<<"2\n";
+        else cout<<"3\n";
+    }
+    else{
+        if(a+b>=3) cout<<"3\n";
+        else cout<<"4\n";
+    }
+    return 0;
+}
