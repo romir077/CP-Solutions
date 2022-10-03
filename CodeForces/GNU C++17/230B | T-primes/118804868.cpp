0dd799f7f24c8f0bf92e920f6b46a0f34321efde
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:46:33 +0530

    Committing files

---
+++ CodeForces/GNU C++17/230B | T-primes/118804868.cpp
@@ -0,0 +1,57 @@
+    #include<bits/stdc++.h>
+    #define ll long long
+    #define rep(i,a,b) for(int i=a;i<b;i++)
+    #define repin rep(i,0,n)
+    #define int long long
+    #define di(a) int a;cin>>a;
+    #define dia di(a)
+    #define dix di(x)
+    #define diy di(y)
+    #define diz di(z)
+    #define dib di(b)
+    #define dic di(c)
+    #define din di(n)
+    #define precise(i) cout<<fixed<<setprecision(i)
+    #define V vector<int>
+    #define vpii vector<pair<int,int>>
+    #define all(x) x.begin(),x.end()
+    #define sis string s;
+    #define sin string s;cin>>s;
+    using namespace std;
+ 
+bool isPrime(int n)
+{
+    // Corner case
+    if (n <= 1)
+        return false;
+  
+    // Check from 2 to n-1
+    for (int i = 2; i*i <= n; i++){
+        if (n % i == 0)return false;
+    }
+    return true;
+}
+ 
+void solve(){
+    din
+    if(ceil(sqrt(n))!=sqrt(n)) {cout<<"NO\n";return;}
+ 
+    int x=(int)sqrt(n);
+    // cout<<x<<" ";
+    // cout<<ceil(sqrt(x))+1<<" ";
+    // cout<<isPrime(x)<<" ";
+    if(isPrime(x)) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
