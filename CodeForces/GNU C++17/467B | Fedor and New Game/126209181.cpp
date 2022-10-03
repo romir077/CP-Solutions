bb36c35ce2cca92ff5e03259e24c2878b3c24086
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:43:51 +0530

    Committing files

---
+++ CodeForces/GNU C++17/467B | Fedor and New Game/126209181.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    din di(m) di(k)
+    V a(m+1);
+    rep(i,0,m+1) cin>>a[i];
+    int tot=0;
+    rep(i,0,m) if(__builtin_popcount(a[i]^a[m])<=k)tot++;
+    cout<<tot;
+    return 0;
+}
