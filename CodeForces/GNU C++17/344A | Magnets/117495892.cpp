0f273da4f1eb4bf5245c68f16f8dd99fd2bde394
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:48:17 +0530

    Committing files

---
+++ CodeForces/GNU C++17/344A | Magnets/117495892.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    din
+    vector<string>a(n);
+    repin cin>>a[i];
+    int g=1;
+    rep(i,0,n-1){
+        if(a[i+1][0]!=a[i][0]) g++;
+    }
+    cout<<g;
+    return 0;
+}
