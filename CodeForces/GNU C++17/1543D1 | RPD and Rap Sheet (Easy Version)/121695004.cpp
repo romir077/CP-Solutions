e305f0fb6cc0d0b48d0b7405b8930e2c16b9b7aa
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:45:10 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1543D1 | RPD and Rap Sheet (Easy Version)/121695004.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+void solve(){
+    din di(k)
+    // cin>>ws;
+    int s=n,d=0,c=1,flag=0;
+    d=((d)^(c));
+    while(s--){
+        if(flag==0) {cout<<0<<endl;flag=1;}
+        else {cout<<d<<endl;c++;d=((c-1)^(c));}
+        // d++;
+        dix
+        if(x==1) break;
+        else continue;
+    }
+    
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
