d14e41e29914f27d0cf686de1052e5a60fde9a7a
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:44:51 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1551B1 | Wonderful Coloring - 1/123466016.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+void solve(){
+    sin
+    map<char,int> x;
+    int n=s.length();
+    repin {
+        if(x.find(s[i])==x.end()){
+            x[s[i]]=1;
+        }
+        else{
+            x[s[i]]++;
+        }
+    }
+    int tot=0,temp=0;
+    for (auto& it : x) {
+        if(it.second>=2) tot++;
+        else temp++;
+    }
+    cout<<temp/2+tot<<"\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
