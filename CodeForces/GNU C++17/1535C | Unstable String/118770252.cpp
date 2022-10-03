eb1d98205e390e75a6b3ec19de80ff8870edfe63
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:46:45 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1535C | Unstable String/118770252.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+void solve(){
+    sin
+    int sum=0,mini=-1;
+    int a[2]={-1,-1};
+    rep(i,0,s.length()){
+        if(s[i]=='1' || s[i]=='0'){
+            a[(s[i]-'0')^(i%2)]=i;
+        }
+        mini=min(a[0],a[1]);
+        sum+=i-mini;
+    }
+    cout<<sum<<"\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
