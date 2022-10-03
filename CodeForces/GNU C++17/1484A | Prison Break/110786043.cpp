bda3b096463820c50043f853c0a422acc5c4a723
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:51:29 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1484A | Prison Break/110786043.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+void solve(){
+    dia dib
+    cout<<a*(b-1)+a<<"\n";
+}
+ 
+signed main()
+{
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
