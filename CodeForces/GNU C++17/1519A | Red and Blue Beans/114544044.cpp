41729a13de7d93e74f7f24d686313dcd1c109671
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:49:39 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1519A | Red and Blue Beans/114544044.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+void solve(){
+    di(r) di(b) di(d);
+    if(min(r,b)*(d+1)>=max(r,b)) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+ 
+signed main()
+{
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
