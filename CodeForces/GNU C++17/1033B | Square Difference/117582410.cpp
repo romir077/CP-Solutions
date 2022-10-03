91ead72906fa016f8f3c6778077022f645c9c0af
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:48:08 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1033B | Square Difference/117582410.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+void solve(){
+    dia dib
+    int c=a-b,d=a+b;
+    int x=0;
+    if(a-b!=1) {cout<<"NO\n";return;}
+    
+    else{
+        rep(i,2,floor(sqrt(d))+1){
+            if(d%i==0) {x=1;break;}
+        }
+        if(x) cout<<"NO\n";
+        else cout<<"YES\n";
+    }
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
