079ad2355280a43f444e49d3dfb5f31d3c9cc3c5
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:35:44 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1631B | Fun with Even Subarrays/144259147.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;
+}
+ 
+void solve(){
+    din V a(n);
+    take(a,n);
+    int x=a.back();
+    int ans=0;
+    int mul=1;
+    for(int i=n-2;i>=0;i--){
+        int y=mul;
+        while(y--){
+            if(i<0){i=-10;break;}
+            if(a[i]!=x){
+                ans++;
+                // cout<<'g'<<i<<" ";
+                i-=(y+1);
+                break;
+            }
+            i--;
+        }
+        if(i>=0){
+            while(a[i]==x){
+                i--;
+                if(i<0)break;
+            }
+        }
+        i++;
+        mul=n-i;
+    }
+    cout<<ans<<"\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
