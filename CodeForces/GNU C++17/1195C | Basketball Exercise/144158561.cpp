5ad7992c4241d19449d0e00326d49d7c077c9ef1
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:35:57 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1195C | Basketball Exercise/144158561.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    din
+    V a(n),b(n);
+    take(a,n);
+    take(b,n);
+    if(n==1){cout<<max(a[0],b[0])<<"\n";return 0;}
+    a[1]+=b[0];
+    b[1]+=a[0];
+    rep(i,2,n){
+        a[i]=max(b[i-1]+a[i],b[i-2]+a[i]);
+        b[i]=max(a[i-1]+b[i],a[i-2]+b[i]);
+    }
+    cout<<max(a[n-1],b[n-1])<<"\n";
+    return 0;
+}
