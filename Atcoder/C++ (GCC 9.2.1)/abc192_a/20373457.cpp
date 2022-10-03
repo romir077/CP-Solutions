8bac4a7520a37256d8471c1828973eceb232134d
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 13:35:18 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc192_a/20373457.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#define ll long long
+#define rep(i,a) for(int i=0;i<a;i++)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define din di(n)
+#define db double
+#define all(x) x.begin(),x.end()
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+signed main()
+{
+    din
+    int x=n%100;
+  cout<<100-x;
+    return 0;
+}
