3750c128da290e33af03a1ece97c873b788a5a27
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:40:43 +0530

    Committing files

---
+++ CodeForces/GNU C++17/463B | Caisa and Pylons/128896173.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    int x=-1;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        if(a[i]>x)x=a[i];
+    }
+    cout<<x<<"\n";
+    return 0;
+}
