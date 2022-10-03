afc9bfe66f012f8b39185372bdd4fe14bd08df0c
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:52:59 +0530

    Committing files

---
+++ CodeForces/GNU C++17/466A | Cheap Travel/107004456.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#define ll long long
+#define rep(i,a) for(int i=0;i<a;i++)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define din di(n)
+#define db double
+#define all(x) x.begin(),x.end()
+using namespace std;
+signed main()
+{
+    din di(m) dia dib
+    if(((double)b/(double)m)<=a){
+    if(n%m==0){
+        cout<<(n/m)*b;
+    }
+    else{
+        if((n-(n/m)*m)*a<=b)
+        cout<<((n/m)*b)+((n-(n/m)*m)*a);
+        else
+        cout<<ceil((double)n/(double)m)*b;
+    }
+    }
+    else{
+        cout<<n*a;
+    }
+    return 0;
+}
