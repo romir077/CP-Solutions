3e22fd25799c4a8d5e8a32f9e27d970357cf980e
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:35:54 +0530

    Committing files

---
+++ CodeForces/GNU C++17/550A | Two Substrings/144161377.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    sin
+    int n=s.length();
+    int x=n+1,y=n+1;
+    rep(i,1,n){
+        if(s[i]=='B' && s[i-1]=='A'){x=i;break;}
+    }
+    rep(i,1,n){
+        if(s[i]=='A' && s[i-1]=='B'){y=i;break;}
+    }
+    bool p=false;
+    // cout<<x<<" "
+    rep(i,x+2,n){
+        if(s[i]=='A' && s[i-1]=='B')p=true;
+    }
+    rep(i,y+2,n){
+        if(s[i]=='B' && s[i-1]=='A')p=true;
+    }
+    if(p)cout<<"YES\n";
+    else cout<<"NO\n";
+    return 0;
+}
