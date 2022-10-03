f265297b9183482b4656db5189cd94e8da6a30fc
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:32:54 +0530

    Committing files

---
+++ CodeForces/GNU C++17/20C | Dijkstra?/147135306.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+#define MOD 1000000007
+#define el cout<<endl;
+using namespace std;
+ 
+map<int,vector<pair<int,int>>>mp;
+map<int,int>pmap;
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    din di(m)
+    rep(i,0,m){
+        dix diy diz
+        mp[x].push_back({y,z});
+        mp[y].push_back({x,z});
+    }
+    V dis(n+1,100000000000000);
+    V vis(n+1,0);
+    dis[1]=0;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+    q.push({0,1});
+    while(!q.empty()){
+        int p=q.top().second;
+        q.pop();
+        if(vis[p]) continue;
+        vis[p]=1;
+        for(auto i:mp[p]){
+            if(dis[i.first]>dis[p]+i.second){
+                dis[i.first]=min(dis[p]+i.second,dis[i.first]);
+                pmap[i.first]=p;
+            }
+            if(!vis[i.first])q.push({dis[i.first],i.first});
+        }
+        
+    }
+    pmap[1]=1;
+    if(dis[n]!=100000000000000){
+        int i=n;
+        V c;
+        c.push_back(n);
+        while(pmap[i]!=1){
+            c.push_back(pmap[i]);
+            i=pmap[i];
+        }
+        c.push_back(1);
+        reverse(all(c));
+        rep(j,0,c.size())cout<<c[j]<<" ";el
+    }
+    else cout<<"-1\n";
+    return 0;
+}
