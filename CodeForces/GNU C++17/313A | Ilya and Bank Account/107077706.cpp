5db04885ed5b3bc1f199b98e6fe8c1aca789f5a2
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:52:37 +0530

    Committing files

---
+++ CodeForces/GNU C++17/313A | Ilya and Bank Account/107077706.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a) for(int i=0;i<a;i++)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define din di(n)
+#define db double
+#define all(x) x.begin(),x.end()
+using namespace std;
+signed main()
+{
+    din
+    if(n<0){
+        cout<<max(n/10,(n/100)*10+n%10);
+    }
+    else{
+        cout<<n;
+    }
+    return 0;
+}
