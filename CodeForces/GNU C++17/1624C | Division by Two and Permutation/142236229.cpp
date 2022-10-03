2bb7838d520b31862ca4b319f39fc0a57d0a9ed0
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:36:26 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1624C | Division by Two and Permutation/142236229.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define take(a,n) for(int j=0;j<n;j++) cin>>a[j];
+#define give(a,n) for(int j=0;j<n;j++) cout<<a[j]<<' ';
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+#define db double
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;
+}
+ 
+void solve(){
+    din
+    V a(n);
+    take(a,n)
+    V arr(n+1,0);
+    bool f=false;
+    repin{
+        int x=a[i];
+        while(x){
+            if(x<=n && x>=1 && arr[x]==0){
+                arr[x]=1;
+                break;
+            }
+            x/=2;
+            if(x==1){
+                if(f){cout<<"NO\n";return;}
+                f=true;
+            }
+        }
+    }
+    if(count(all(arr),1)==n){cout<<"YES\n";}
+    else cout<<"NO\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(t)
+    while(t--){
+        solve();
+    }
+    return 0;
+}
