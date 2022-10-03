7929e269bee6d99cc370dbf5b05ca558ae24f052
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:54:38 +0530

    Committing files

---
+++ CodeForces/GNU C++17/339A | Helpful Maths/105002680.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#define rep(i,a) for(int i=0;i<a;i++)
+using namespace std;
+ 
+int main()
+{
+    string s;
+    cin>>s;
+    int n=s.length();
+    int l=(n+1)/2;
+    int p[l];
+    for(int i=0;i<l;i++){
+        p[i]=(s[(2*i)]-48);
+    }
+    sort(p,p+l);
+    for(int j=0;j<l-1;j++){
+        cout<<p[j]<<'+';
+    }
+    cout<<p[l-1];
+ 
+    return 0;
+}
