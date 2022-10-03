8383389bc8293cfa7bb38dd4c26c8a169f4d4cfb
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:52:22 +0530

    Committing files

---
+++ CodeForces/GNU C++17/441A | Valera and Antique Items/107164459.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#define ll long long
+#define rep(i,a) for(int i=0;i<a;i++)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define din di(n)
+#define db double
+#define all(x) x.begin(),x.end()
+using namespace std;
+signed main()
+{
+    din di(v)
+    vector<int> t;
+    rep(i,n){
+        dix
+        int arr[x];
+        rep(j,x){
+            cin>>arr[j];
+        }
+        sort(arr,arr+x);
+        if(v>arr[0]){
+            t.push_back(i+1);
+        }
+    }
+    cout<<t.size()<<endl;
+    rep(i,t.size()){
+        cout<<t[i]<<" ";
+    }
+    return 0;
+}
