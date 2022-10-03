2961377a6edc256070480e20624d8a25f645956b
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:43:34 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1560A | Dislike of Threes/126378772.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    V a(1000);
+    int x=0;
+    rep(i,1,1667){
+        if(i%3!=0 && i%10!=3) {a[x]=i;x++;} 
+    }
+    din
+    while(n--){
+        di(t)
+        cout<<a[t-1]<<"\n";
+    }
+    return 0;
+}
