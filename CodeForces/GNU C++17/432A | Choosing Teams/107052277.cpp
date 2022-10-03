7959d645f0bb6b61c0b00f4b34d3ec19936f746d
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:52:50 +0530

    Committing files

---
+++ CodeForces/GNU C++17/432A | Choosing Teams/107052277.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a) for(int i=0;i<a;i++)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    din di(k)
+    int arr[n],count=0;
+    rep(i,n){
+        cin>>arr[i];
+        if(k+arr[i]<=5)
+        count++;
+    }
+    cout<<count/3;
+    return 0;
+}
