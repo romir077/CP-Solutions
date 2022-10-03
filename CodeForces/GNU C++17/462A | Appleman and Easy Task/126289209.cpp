633deb4601da44c93738bc08f52c223c5f5821c1
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:43:40 +0530

    Committing files

---
+++ CodeForces/GNU C++17/462A | Appleman and Easy Task/126289209.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power(int a,int b){
+    int x=1;
+    while(b--) x*=a;
+    return x;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    din
+    string r="";
+    repin {sin r+=s;}
+    string s=r;
+    reverse(all(s));
+    cout<<((r==s)?"YES\n":"NO\n");
+    return 0;
+}
