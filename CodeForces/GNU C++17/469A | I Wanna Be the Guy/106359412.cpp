7a3af29f286af84af7477ad803cb093bdf1f9b52
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:53:13 +0530

    Committing files

---
+++ CodeForces/GNU C++17/469A | I Wanna Be the Guy/106359412.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a) for(int i=0;i<a;i++)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    din
+    dix
+    vector <int> main={0};
+    int arr[x];
+    rep(i,x){
+        cin>>arr[i];
+            int flag=0;
+            rep(j,main.size()){
+                if(arr[i]!=main[j]){
+                   // cout<<"X";
+                    continue;
+                }
+                flag=1;
+                //cout<<"Y";
+            }
+          //  cout<<"Z";
+            if(flag!=1){
+            main.push_back(arr[i]);
+           // cout<<"A";
+            }
+    }        
+    
+    diy
+    int brr[y];
+    rep(i,y){
+        cin>>brr[i];
+            int flag=0;
+            rep(j,main.size()){
+                if(brr[i]!=main[j]){
+                   // cout<<"X";
+                    continue;
+                }
+                flag=1;
+                //cout<<"Y";
+            }
+          //  cout<<"Z";
+            if(flag!=1){
+            main.push_back(brr[i]);
+           // cout<<"A";
+            }
+    }
+    if(main.size()-1==n){
+        cout<<"I become the guy."<<"\n";
+    }
+    else{
+        cout<<"Oh, my keyboard!"<<"\n";
+    }
+    return 0;
+}
