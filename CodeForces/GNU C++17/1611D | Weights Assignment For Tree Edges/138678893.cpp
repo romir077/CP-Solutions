ac3fb7bf8eefa038bd80e072427311b45454ed4b
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:38:05 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1611D | Weights Assignment For Tree Edges/138678893.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> b(n + 1), p(n + 1), dist(n + 1, -1);
+ 
+    for(int i = 1; i <= n; i++)
+        cin >> b[i];
+    for(int i = 1; i <= n; i++)
+        cin >> p[i];
+ 
+    if (b[p[1]] != p[1]){
+        cout << -1 << '\n';
+        return;
+    }
+ 
+    dist[p[1]] = 0;
+    for(int i = 2; i <= n; i++){
+        if(dist[b[p[i]]] == -1){
+            cout << -1 << '\n';
+            return;
+        }
+        dist[p[i]] = dist[p[i - 1]] + 1;
+    }
+ 
+    for(int i = 1; i <= n; i++) {
+        cout << dist[i] - dist[b[i]] << ' ';
+    }
+    cout << '\n';
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while(t-- > 0) {
+        solve();
+    }
+}
