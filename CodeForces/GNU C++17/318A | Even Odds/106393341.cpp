e59535e01454608e8a4ce078832390a624529af1
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:53:09 +0530

    Committing files

---
+++ CodeForces/GNU C++17/318A | Even Odds/106393341.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a) for(int i=0;i<a;i++)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    dix diy
+    // int z[x],a=0;
+    // for(int i=1;i<=x;i++){
+    //     if((i)%2==1){
+    //         z[a];
+    //         a++;
+    //     }   
+    // }
+    if(y<=ceil((double)x/2)){
+        cout<<(int)(2*y-1);
+    }
+    else{
+        cout<<(int)(2*(y-ceil((double)x/2)));
+    }
+    // for(int i=1;i<=x;i++){
+    //     if((i)%2==0){
+    //         z[a];
+    //         a++;
+    //     }
+    // }
+    // cout<<z[y-1];
+    return 0;
+}
