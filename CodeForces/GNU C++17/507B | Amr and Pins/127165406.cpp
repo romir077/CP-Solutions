727510c415ab5faa9c29c8a129c2877dffc21b7c
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:42:49 +0530

    Committing files

---
+++ CodeForces/GNU C++17/507B | Amr and Pins/127165406.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    di(r) dia dib dix diy
+    int s=2*r;
+    double z=sqrt((x-a)*(x-a)+(y-b)*(y-b));
+    int f=ceil(z/(double)s);
+    cout<<f<<"\n";
+    return 0;
+}
