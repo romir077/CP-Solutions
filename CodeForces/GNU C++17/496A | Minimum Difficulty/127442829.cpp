766d8c4fd43f1d6fa41e35566454a648c1efbdae
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:42:18 +0530

    Committing files

---
+++ CodeForces/GNU C++17/496A | Minimum Difficulty/127442829.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+ 
+int power( int N, int M){
+    int power = N, sum = 1;
+    if(N == 0) sum = 0;
+    while(M > 0){if((M & 1) == 1){sum *= power;}
+    power = power * power;M = M >> 1;}
+    return sum;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    din
+    V a(n);
+    int x=INT_MAX;
+    repin cin>>a[i];
+    rep(i,1,n-1){
+        if(a[i+1]-a[i-1]<x)x=a[i+1]-a[i-1];
+    }
+    int y=a[1]-a[0];
+    rep(i,1,n-1){
+        y=max(y,a[i+1]-a[i]);
+    }
+    cout<<max(x,y);
+    return 0;
+}
