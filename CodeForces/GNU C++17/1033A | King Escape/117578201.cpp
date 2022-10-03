3001c5b6c1a17017c4605d3940f3014131135468
romir077 <76393093+romir077@users.noreply.github.com>
2022-10-03 12:48:11 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1033A | King Escape/117578201.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repin rep(i,0,n)
+#define int long long
+#define di(a) int a;cin>>a;
+#define dia di(a)
+#define dix di(x)
+#define diy di(y)
+#define diz di(z)
+#define dib di(b)
+#define dic di(c)
+#define din di(n)
+#define precise(i) cout<<fixed<<setprecision(i)
+#define V vector<int>
+#define vpii vector<pair<int,int>>
+#define all(x) x.begin(),x.end()
+#define sis string s;
+#define sin string s;cin>>s;
+using namespace std;
+ 
+signed main()
+{
+    din
+    dia dib
+    dix diy
+    di(p) di(q)
+    if((b-y)*(b-q)<=0 || (a-x)*(a-p)<=0) cout<<"NO\n";
+    else cout<<"YES\n";
+    return 0;
+}
